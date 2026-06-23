69d7bc05181e58b4da8b26b847fff100ed28cc07
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-23 10:47:07 +0200

    señales

--- curso/src/app/ejemplos/demos/demos.ts
+++ curso/src/app/ejemplos/demos/demos.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, computed, effect, resource, signal } from '@angular/core';
 
 @Component({
   selector: 'app-demos',
-  imports: [],
+  imports: [ JsonPipe],
   templateUrl: './demos.html',
   styleUrl: './demos.css',
 })
-export class Demos {}
+export class Demos {
+  // ejemplo de señales
+  readonly conSignal = signal(0)
+  readonly doble = computed(() => this.conSignal() * 2)
+  sinSignal = 0
+
+  intervalos: number[] = []
+
+  constructor() {
+    this.intervalos.push(setInterval(() => this.conSignal.update(value => value + 1), 3_000))
+    this.intervalos.push(setInterval(() => {this.sinSignal++; console.warn(this.sinSignal)}, 1_000))
+    effect(() => {
+      console.log(`Contador: ${this.conSignal()}`)
+    })
+  }
+
+  ngOnDestroy() {
+    this.intervalos.forEach(intervalo => clearInterval(intervalo))
+  }
+
+  addSignal() {
+    this.conSignal.update(value => value + 1)
+  }
+  addSinSignal() {
+    this.sinSignal++
+  }
+
+  readonly file = signal<Blob | undefined>(undefined)
+  readonly lector = resource({
+    params: () => ({ file: this.file() }),
+    loader: ({ params }) => new Promise((resolve, reject) => {
+      if (!params.file) reject('Falta el fichero')
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = () => reject(reader.error);
+      reader.readAsText(params.file as Blob);
+    })
+  })
+
+}
